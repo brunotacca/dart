ad510327fe533b6fd410a479262b0020efe1a3e3
Bruno Tacca <brunotacca@gmail.com>
2020-07-22 18:38:06 -0300

    .

--- 0_Intro/11_operators.dart
+++ 0_Intro/11_operators.dart
@@ -1,6 +1,5 @@
 main() {
-
-  int num = 10+22;
+  int num = 10 + 22;
   num = num - 2;
   print(num);
 
@@ -8,7 +7,7 @@ main() {
   print(num);
 
   // relational ==, !=, >=, <=
-  if(num==0) print('Zero');
+  if (num == 0) print('Zero');
 
   num = 100;
   num += 2;
@@ -16,22 +15,22 @@ main() {
 
   print(++num); // 103
   print(num++); // 103
-  print(num+=1); // 105
-  print(num-=1); // 104
+  print(num += 1); // 105
+  print(num -= 1); // 104
 
   print(num); // 104
 
-  if(num > 200 && num < 203) print('201 to 202');
+  if (num > 200 && num < 203) print('201 to 202');
 
-  if(num != 100) print('not 100');
+  if (num != 100) print('not 100');
 
   var n1 = Num();
   int number;
-  if(n1!=null) number = n1.numb;
+  if (n1 != null) number = n1.numb;
   print(number);
 
   var n2;
-  if(n2!=null) number = n2.numb; // doesn't enter here.
+  if (n2 != null) number = n2.numb; // doesn't enter here.
   print(number);
 
   // number = n2.num; -->> NoSuchMethodError: The getter 'num' was called on null.
@@ -49,7 +48,7 @@ main() {
 
   n5 = n4?.num ?? 0; // default to 0 instead of null if n4 is null
   print(n5);
-  
+
   print(n4); // null
   print(n4 ??= 100); // default-assign 100 if n4 is null.
   print(n4);
@@ -70,6 +69,13 @@ main() {
   var isNull = null;
   print(isNull?.foo()); // null (does not throw exception)
 
+  int n6;
+  int n7 = 10;
+  int n8 = 20;
+  n8 = n6 ?? n8;
+  print(n8); // prints 20
+  n8 = n7 ?? n8;
+  print(n8); // prints 10
 }
 
 class Num {
